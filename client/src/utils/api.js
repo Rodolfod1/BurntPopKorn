4125fd71630ee661a8626f939270ee1f37a4218a
Brian Smak <brian.j.smak@gmail.com>
2021-02-11 21:54:06 -0500

    updated axios api.js call

---
+++ client/src/utils/api.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+//import REACT_APP_APIKEY from ""
+const APIKEY = process.env.REACT_APP_OMDB_APIKEY;
+const BASEURL = "http://www.omdbapi.com/?i=tt3896198&apikey=" + APIKEY;
+
+console.log(BASEURL);
+axios.get(BASEURL).then(res => {console.log(res.json)})
+
+
+
