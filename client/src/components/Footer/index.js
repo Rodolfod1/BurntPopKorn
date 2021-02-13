8362cfedde6bc959772c250adaf27f6a9a4fcdec
Megan <carnagh5@gmail.com>
2021-02-13 14:19:35 -0500

    added footer component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,1 +1,19 @@
 import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <img className="footer__img" src="logo-no-text.png" alt="logo" />
+
+      <div className="footer__contactDiv">
+        <h4 className="footer__h4">Created By</h4>
+        <p className="footer__p">Megan Carnaghi</p>
+        <p className="footer__p">Rodolfo Diaz</p>
+        <p className="footer__p">Daniel Hawthorne-Foss</p>
+        <p className="footer__p">Brian Smak</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
