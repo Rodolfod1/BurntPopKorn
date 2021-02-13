778d8078ebd74d6eaf940def9fe028ec658e1377
Brian Smak <brian.j.smak@gmail.com>
2021-02-13 13:31:41 -0500

    added package fixed route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import Home from './pages/home'
-import SignIn from './pages/signIn'
-import Profile from './pages/profile'
+//import SignIn from './pages/signIn'
+//import Profile from './pages/profile'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './utils/api';
 
@@ -9,16 +9,7 @@ function App() {
     <Router>
     <div>
     <Route exact path="/">
-    <SignIn />
-    </Route>
-    <Route exact path="/home">
     <Home />
-    </Route>
-    <Route exact path="/home">
-    <Home />
-    </Route>
-    <Route exact path="/profile">
-    <Profile />
     </Route>
     </div>
     </Router>
