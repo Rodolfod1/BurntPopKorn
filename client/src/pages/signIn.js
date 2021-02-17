792a5fb647ef120387cb87db77f705caed118538
Megan <carnagh5@gmail.com>
2021-02-16 20:08:38 -0500

    updated header component and signin page

--- client/src/pages/signIn.js
+++ client/src/pages/signIn.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "../components/HeaderSignIn/HeaderSignIn.css";
+import "../components/SignInForm/SignInForm.css";
+import "../components/Footer/Footer.css";
+import HeaderSignIn from "../components/HeaderSignIn";
+import SignInForm from "../components/SignInForm";
+import Footer from "../components/Footer";
+import "./home.css";
 
-
-function signIn() {
-    return(<Link to="/">GO TO MAIN</Link>)
+function SignIn() {
+  return (
+    <div className="signinpage__container">
+      <Link to="/">GO TO MAIN</Link>
+      <HeaderSignIn />
+      <SignInForm />
+      <Footer />
+    </div>
+  );
 }
 
-
-export default signIn
+export default SignIn;
