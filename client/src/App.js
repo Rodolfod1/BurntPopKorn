cf369b5734bf05928251bfe7dc2ff392801811e8
Brian Smak <brian.j.smak@gmail.com>
2021-02-16 19:57:36 -0500

    updated route to include signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useContext } from "react";
 import { AuthContext } from "./authentication/AuthContext";
 import Home from "./pages/home";
-//import SignIn from './pages/signIn'
+import SignIn from './pages/signIn'
 //import Profile from './pages/profile'
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./utils/api";
 import "./index.css";
 
@@ -16,9 +16,14 @@ function App() {
   return (
     <Router>
       <div>
+        <Switch>
         <Route exact path="/">
           <Home />
         </Route>
+        <Route exact path="/signin">
+          <SignIn />
+        </Route>
+        </Switch>
       </div>
     </Router>
   );
--- client/src/pages/signIn.js
+++ client/src/pages/signIn.js
@@ -1,1 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+
+
+function signIn() {
+    return(<Link to="/">GO TO MAIN</Link>)
+}
+
+
+export default signIn
+
