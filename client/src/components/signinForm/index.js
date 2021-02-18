187626d197e2eea49042c141c09aff0cd02e2a0d
Daniel <dhfoss@gmail.com>
2021-02-18 16:10:42 -0500

    Sign in errors appear.  If sign in is succesful, user is redirected to the login page

--- client/src/components/signinForm/index.js
+++ client/src/components/signinForm/index.js
@@ -1,19 +1,24 @@
 import React, { useState, useContext, useRef } from "react";
+import { useHistory } from 'react-router-dom';
 import { AuthService } from "../../authentication/AuthService";
 import { AuthContext } from "../../authentication/AuthContext";
 
 function SignInForm(props) {
+  const history = useHistory();
   const authContext = useContext(AuthContext);
   const usernameRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
+  const [alertMessage, setAlertMessage] = useState(null)
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (!usernameRef.current.value) {
+      setAlertMessage('Please enter a username and password');
       return;
     }
     if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      setAlertMessage('Password fields do not match')
       return;
     }
     AuthService.register({
@@ -21,6 +26,11 @@ function SignInForm(props) {
       password: passwordRef.current.value,
     }).then((data) => {
       console.log(data);
+      if (data.message.msgError) {
+        setAlertMessage(data.message.msgBody);
+      } else {
+        history.push('/login');
+      }
     });
   };
 
@@ -37,6 +47,18 @@ function SignInForm(props) {
 
         <form className="form" onSubmit={onSubmit}>
           <label htmlFor="uname"></label>
+
+
+
+          {/* This div is the alert that pops up and tells the user if they didn't complete the form,
+          if their passwords don't match, or if their username is already taken
+          It needs to be styled to look scary and intimidating */}
+          <div className="message" >
+            {alertMessage}
+          </div>
+
+
+
           <input
             className="form__input"
             type="text"
