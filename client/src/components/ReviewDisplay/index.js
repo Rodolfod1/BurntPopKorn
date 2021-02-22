f64a8a8a4cee89429508295650935b4001a2a466
Megan <carnagh5@gmail.com>
2021-02-22 16:38:52 -0500

    styled movie review section

--- client/src/components/ReviewDisplay/index.js
+++ client/src/components/ReviewDisplay/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './ReviewDisplay.css'
+import React from "react";
+import "./ReviewDisplay.css";
 
-const ReviewDisplay = props => {
-    return (
-        <div>
-            <p>Title: {props.info.title}</p>
-            <p>Your Rating: {props.info.userRating}</p>
-            <p>Your Review: {props.info.review}</p>
-        </div>
-    )
-}
+const ReviewDisplay = (props) => {
+  return (
+    <div className="reviews__selectedreview">
+      <div className="selectedreview__div">
+        <h4 className="selectedreview__heading">Title:</h4>{" "}
+        <span className="selectedreview__span">{props.info.title}</span>
+      </div>
+      <div className="selectedreview__div">
+        <h4 className="selectedreview__heading">Your Rating:</h4>{" "}
+        <span className="selectedreview__span">{props.info.userRating}</span>
+      </div>
+      <div className="selectedreview__div">
+        <h4 className="selectedreview__heading">Your Review:</h4>{" "}
+        <span className="selectedreview__span">"{props.info.review}"</span>
+      </div>
+    </div>
+  );
+};
 
 export default ReviewDisplay;
