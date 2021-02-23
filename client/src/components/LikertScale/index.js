ff82268b0bbb82a343c8d7f929c17cf305678bb6
Megan <carnagh5@gmail.com>
2021-02-22 21:00:04 -0500

    Added images for burnt meter

--- client/src/components/LikertScale/index.js
+++ client/src/components/LikertScale/index.js
@@ -3,14 +3,14 @@ import Likert from "react-likert-scale";
 
 function LikertScale() {
   const likertOptions = {
-    question:
-      "On a scale of burnt and disgusting, to popped, buttery goodness, what was your opinion of the movie?",
+    // question:
+    //   "On a scale of 1-5 (from burnt and disgusting, to popped, buttery goodness), what was your opinion of the movie?",
     responses: [
-      { value: 1, text: "Abysmal" },
-      { value: 2, text: "Poor" },
-      { value: 3, text: "Average", checked: true },
-      { value: 4, text: "Good" },
-      { value: 5, text: "Excellent" },
+      { value: 1, text: "1" },
+      { value: 2, text: "2" },
+      { value: 3, text: "3", checked: true },
+      { value: 4, text: "4" },
+      { value: 5, text: "5" },
     ],
     onChange: (val) => {
       console.log(val);
