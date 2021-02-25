06fece000f36d324e4c73ce80a76e1144516b247
Daniel <dhfoss@gmail.com>
2021-02-24 20:15:40 -0500

    Favorite icons render conditionally

--- client/src/components/MovieItem/index.js
+++ client/src/components/MovieItem/index.js
@@ -3,9 +3,10 @@ import "./MovieItem.css";
 import star from "../images/star.png";
 
 const MovieItem = (props) => {
+  console.log(props.movie.favorite)
   return (
     <li className="reviews__listItem">
-      <img className="reviews__ticket" src={star} alt="movie ticket" />
+      {!props.movie.favorite ? <></> : <img className="reviews__ticket" src={star} alt="movie ticket" />}
       {props.movie.title}
       <button
         className="reviews__button"
