6c4eaa0f27099bb9d6f7b171f276218396a14950
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-02-28 00:35:28 -0500

    refactoring code for canvasjs

--- client/src/pages/stats.js
+++ client/src/pages/stats.js
@@ -3,7 +3,7 @@ import HeaderStats from "../components/HeaderStats";
 import BarGraph from "../components/BarGraph";
 import DoughnutChart from "../components/DoughnutChart";
 import BetterDonut from "../components/BetterDonut";
-
+// BetterDonut will replace the DoughnutChart to include the labels , R. Diaz 
 function Stats() {
   return (
     <div>
